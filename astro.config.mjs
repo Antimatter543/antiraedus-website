a60470d87ec297f112aa62dcfaaf236673348126
Anti <spiralknightsowner@gmail.com>
2025-07-05 13:19:04 +1000

    Static build added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/antiraedus-website/',
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: true
     }
   },
-  site: 'https://yourwebsite.com',
+  // site: 'https://yourwebsite.com',
   integrations: []
 });
