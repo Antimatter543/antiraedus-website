d1ad333e46e80888de3e89d76d32676ffccecd10
Antimatter543 <antimatter543@users.noreply.github.com>
2025-08-26 16:12:56 +1000

    Added katex rendering into it again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'one-dark-pro',
       wrap: true
