70a68453ab0ac96c9f52f1938019c6538b9b3e23
Antimatter543 <antimatter543@users.noreply.github.com>
2025-08-26 16:20:45 +1000

    almost had table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
+import remarkToc from 'remark-toc';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
   markdown: {
-    remarkPlugins: [remarkMath],
+    remarkPlugins: [remarkMath, [remarkToc, { heading: ':toc', tight: true }]],
     rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'one-dark-pro',
